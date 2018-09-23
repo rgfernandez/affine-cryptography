945dfe6abc22e1767d1ef3d8fff39b8e33af34d7
Randy Joseph Fernandez <rgfernandez@ieee.org>
2018-09-23 21:23:47 +0800

    added error checking

--- euclidean.py
+++ euclidean.py
@@ -1,14 +1,14 @@
-def gcd(a,b):
-	if b == 0:
+def gcd(a, b):
+	c = max(a, b)
+	d = min(a, b)
+
+	if d == 0:
 		return a
 
-	return gcd(b, a%b)
+	return gcd(d, c%d)
 
 if __name__ == '__main__':
 	a = input('Enter first number: ')
 	b = input('Enter second number: ')
-	if a > b:
-		c = gcd(int(a),int(b))
-	else:
-		c = gcd(int(b),int(a))
+	c = gcd(int(a),int(b))
 	print(f'The GCD of {a} and {b} is {c}')
