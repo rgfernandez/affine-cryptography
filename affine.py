535c5c6c11f2284a384c29a2af069a29361f9fcb
Randy Joseph Fernandez <rgfernandez@ieee.org>
2018-09-23 23:30:07 +0800

    added affine cipher

---
+++ affine.py
@@ -0,0 +1,33 @@
+from extended_euclidean import extended_gcd
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def affine_encrypt(msg, a, b):
+	result = ''
+	for text in msg:
+		index = letters.find(text)
+		result += letters[(a * index + b) % 26]
+	return result
+
+def affine_decrypt(msg, a, b):
+	result = ''
+	for text in msg:
+		index = letters.find(text)
+		(r, s, t) = extended_gcd(26, a)
+		result += letters[(t % 26) * (index - b) % 26]
+	return result
+
+if __name__ == '__main__':
+	resp = input('[E]ncrypt or [D]ecrypt? ')
+	if resp not in ('E', 'D'):
+		print('Not recognized')
+
+	msg = input('Type message: ')
+	a = int(input('Type a: '))
+	b = int(input('Type b: '))
+
+	if resp == 'E':
+		print(affine_encrypt(msg.upper(), a, b))
+	else:
+		print(affine_decrypt(msg.upper(), a, b))
+	
